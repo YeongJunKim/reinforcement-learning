55f3eda33e6f82f893ff12ce56cd09d897cb0669
colson <dud3722000@naver.com>
2019-08-30 21:52:51 +0900

    .

--- 4. gamblers/main.m
+++ 4. gamblers/main.m
@@ -1,12 +1,18 @@
-
 clc
+clf
 clear all
 %% Value Iteration
 % gamblers_problem
+% value 그래프를 보면, 내가 돈을 많ㅇ ㅣ가지고 있을 수록 이길 확률이 높다.
+% policy 그래프가 이상하게 나오는데 이는
+% 동전을 던지면서 게임을 해나갈 수록, 점점 질 확률이 높아지는 게임이므로,
+% 최대한 게임을 덜하고 이기는 것을 목표로 하기에 이렇게 나옴.
+% HEAD_PROB 를 바꿔가면서 해보자
+% http://www.modulabs.co.kr/RL_Practice/10005 모두연의 명 설명
 
 data.GOAL = 100;
 data.STATE = zeros(data.GOAL+1, 1);
-data.HEAD_PROB = 0.4;
+data.HEAD_PROB = 0.6;
 data.BACK_PROB = 0.6;
 
 data
@@ -41,7 +47,7 @@ while(true)
         state_value(data.GOAL+1, 1) = 1.0;
    end
    delta = max(abs(state_value - old_state_value));
-   if delta < 0.0000000000001
+   if delta < 0.000000001
         sweeps_history(:, cnt) = state_value;
         cnt = cnt + 1;
        break;
@@ -51,8 +57,9 @@ end
 subplot(1,2,1);
 cnt
 for index = 1 : cnt-1
-    plot(sweeps_history(:,index));
     hold on;
+    plot(sweeps_history(:,index));
+    
 end
 
 % compute the optimal policy
